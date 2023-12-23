3ea9074550d64afd7b5988eaba7cdd6aec0be0b9
wassim <mohamedbehiani15@gmail.com>
2023-12-23 03:55:42 +0100

    Set up the initial vue app

---
+++ editor/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <p>Hello vue comunity</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
